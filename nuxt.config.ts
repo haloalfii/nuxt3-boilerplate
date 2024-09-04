997693b60ef3181a6be7bdc2f37492254d61eacb
Alfian <alfiankurniawan85@gmail.com>
2024-09-04 20:50:44 +0700

    Initial Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+
+  tailwindcss: {
+    cssPath: '~/assets/css/input.css'
+  },
+  
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js',
+          type: 'text/javascript',
+          defer: true,
+        },
+      ],
+    },
+  }
+})
