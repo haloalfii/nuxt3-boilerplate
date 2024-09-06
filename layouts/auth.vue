dfbc143b36b23b495655260d1adb975951f4a66a
Alfian Luthfi <alfiankurniawan85@gmail.com>
2024-09-06 14:22:15 +0700

    Adding Primary Blue Style

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -5,7 +5,31 @@
 </template>
 
 <script setup>
+import { storeToRefs } from 'pinia'
+import { useFlowbite } from '~/composables/useFlowbite';
+const global = useGlobal()
 
+const { getMode } = storeToRefs(global)
+const { setMode } = global
+
+onMounted(async () => {
+    const html = document.body
+    const mode = localStorage.getItem('theme')
+
+    if (mode === 'dark') {
+        localStorage.theme = 'dark'
+        setMode('dark')
+        html.classList.add('dark')
+    } else {
+        localStorage.theme = 'light'
+        setMode('light')
+        html.classList.remove('dark')
+    }
+
+    useFlowbite(() => {
+        initFlowbite();
+    })
+})
 </script>
 
 <style lang="scss" scoped>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,13 @@
 import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
+  theme: {
+    extend: {
+      colors: {
+        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a", "950": "#172554" }
+      }
+    },
+  },
   content: [
     "./node_modules/flowbite/**/*.{js,ts}",
     "./components/**/*.{js,vue,ts}",
